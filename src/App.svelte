0143c10473d062259d2e8bcfb0348f2261a2e334
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-01-20 10:58:00 +0530

    fix: css and js

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
 {:else}	
 	<div class="cover">
 		<h1 class="m-0">Ludo</h1>
-		<p class="m-0">A simple site to play Ludo (Pachisi Variant)!</p>
+		<p class="m-0">A site built in svelte to play singleplayer ludo for fun even with bots!</p>
 
 		<a href="?game" class="cover-btn">Lets play!</a>
 		<p class="credits m-0">Made by <a href="https://github.com/scientific-dev">scientific-dev</a></p>
@@ -38,6 +38,7 @@
 
 	.cover h1 {
 		font-size: 80px;
+		margin-left: -4px;
 	}
 
 	.cover p {
@@ -77,7 +78,7 @@
 		}
 	
 		.cover h1 {
-			line-height: 1;
+			line-height: 1.25;
 			margin-top: 30px;
 		}
 
